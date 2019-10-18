9a6a98ba1e80621f5fa52058d54161fcd179e2c0
carlosrodri <carlos.rodriguez12@uptc.edu.co>
2019-10-18 17:41:52 -0500

    starting repository

---
+++ MyPerfyl/src/app/back-button/back-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'my-back-button',
+  templateUrl: './back-button.component.html',
+  styleUrls: ['./back-button.component.scss'],
+})
+export class BackButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ MyPerfyl/src/app/tab1/tab1.page.ts
@@ -0,0 +1,14 @@
+import { BackButtonComponent } from './../back-button/back-button.component';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+})
+
+export class Tab1Page {
+
+  constructor() {}
+
+}
